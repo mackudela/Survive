cc49a6893725a777472e63c2c811f32e1478f4a0
Maciej Kudela <mackudela@gmail.com>
2023-06-07 15:33:27 +0200

    Initializing project files

---
+++ Survive/Survive/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
